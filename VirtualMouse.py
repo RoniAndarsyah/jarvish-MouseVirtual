6d54cfa0e6b7460e7ed81bda1d08dbf741c3ff27
argyarijal19 <73386224+argyarijal19@users.noreply.github.com>
2021-11-24 17:01:32 +0700

    commit -m 'cek the handlandmark function'

--- VirtualMouse.py
+++ VirtualMouse.py
@@ -1,7 +1,6 @@
 import time
-
+from cvzone.HandTrackingModule import HandDetector
 import cv2
-import pyHandTrackingModule as htm
 import numpy as np
 
 import autopy
@@ -16,10 +15,19 @@ plocx, ploxy = 0, 0
 cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
-detector = htm.handDetector(maxhands=2)
-wscr, hscr = autopy.screen.size()
+detector = HandDetector(detectionCon=0.8, maxHands=2)
 
 while True:
     seccess, img = cap.read()
-    img = detector.findHands(img)
-    lmList, bbox = detector.findPosition(img)
+    Hands, img = detector.findHands(img)
+    # cek function hand lancmark
+    if len(Hands):
+        hand1 = Hands[0]
+        lmList1 = hand1["lmlist"]  # list dari 21 landmark point
+        bbox1 = hand1["bbox"]  # info dari index box x,y,w,h
+        centerPoint1 = hand1["center"]  # titik tengah dari tangan cx, cy
+        handType = hand1["type"]  # cek tangan right or left
+
+    print(len(lmList1))
+    cv2.imshow("image", img)
+    cv2.waitKey(1)
