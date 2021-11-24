f240b666c4c5843b0a0511724bf8bd304c92ad88
argyarijal19 <73386224+argyarijal19@users.noreply.github.com>
2021-11-24 17:11:09 +0700

    'cek function handlandmark'

--- VirtualMouse.py
+++ VirtualMouse.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import time
 from cvzone.HandTrackingModule import HandDetector
 import cv2
@@ -32,30 +31,3 @@ while True:
     print(len(lmList1))
     cv2.imshow("image", img)
     cv2.waitKey(1)
-=======
-import time
-
-import cv2
-import HandTrackingModule as htm
-import numpy as np
-
-import autopy
-
-wCam, hCam = 640, 480
-frmeR = 100
-
-pTime = 0
-plocx, ploxy = 0, 0
-plocx, ploxy = 0, 0
-
-cap = cv2.VideoCapture(0)
-cap.set(3, wCam)
-cap.set(4, hCam)
-detector = htm.handDetector(maxhands=2)
-wscr, hscr = autopy.screen.size()
-
-while True:
-    seccess, img = cap.read()
-    img = detector.findHands(img)
-    lmList, bbox = detector.findPosition(img)
->>>>>>> ce9694bf4a07c8e49328291285d1fa74b25a243f
